60d6168f85c12546ba02d5d099b34b9a2016e1c7
gyurili <inglifestora@naver.com>
2024-10-08 12:45:53 +0900

    테스트

--- week2/Project1/Project1/1.C
+++ week2/Project1/Project1/1.C
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("안녕하세요!");
+
+    printf("Hello"); // 한 줄 주석
+
+    /*
+    여러줄에 걸친
+    주석을 달 수 있습니다.
+    */
 
     return 0;
 }
--- week2/Project1/Project2/2.c
+++ week2/Project1/Project2/2.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("안녕하세요~");
+    
+    int number1, number2;
+    number1 = 1;
+    number2 = 2;
+
+    int number3 = 3;
+
+    printf("%d\n", number1);
+    printf("%d", number2);
+    printf("%d원", number3);
 
     return 0;
 }
